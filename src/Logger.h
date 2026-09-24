92acf7d790655552bdf5916daf04bbecd01f083b
JiaHaoZhao05 <jia.hao.zhao@estudiantat.upc.edu>
2026-09-24 11:51:52 +0200

    made it so that cpp can be executed

--- src/Logger.h
+++ src/Logger.h
@@ -1,47 +1,24 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <iostream>
-// OpenGL
-#include <glad/glad.h>
-#include <SDL3/SDL.h>
-// GLM
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-// ImGui
-#include "imgui.h"
-#include "imgui_impl_sdl3.h"
-#include "imgui_impl_opengl3.h"
 
-#include "ImGuizmo.h"
 class Log {
 public:
 	Log() {}
 	~Log() {}
 
 	//Add a message to the log
-	void LOG(std::string message) {
-		msgLog.push_back(message);
-	}
+	void LOG(std::string message);
 
 	//Clean the log
-	void Clear() {
-		msgLog.clear();
-	}
+	void Clear();
 
 	//Draw the console window where the log is displayed
-	void Log::DrawConsole() {
-		//Console window
-		ImGui::Begin("Console", nullptr);
-		for (const auto& a : msgLog) {
-			ImGui::Text(a.c_str());
-		}
-		ImGui::End();
-	}
+	void Log::DrawConsole();
 
 	//Get the messages from the log
 	const std::vector<std::string>& GetMessages() const {return msgLog;	}
 private:
+	//all the messages from the log
 	inline static std::vector<std::string> msgLog;
 };
